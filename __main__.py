4a59f8e7ab1d4607292113a0726f31104ffb9bec
Tokisaki-Galaxy <61621971+Tokisaki-Galaxy@users.noreply.github.com>
2024-03-31 00:19:30 +0800

    完善一些功能，加入被点名提示，作答提示，自动发弹幕

--- __main__.py
+++ __main__.py
@@ -12,6 +12,8 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+dmsg=0 #上一条信息，防止大量重复发送
+
 changjiang = {
     "changjiang_user": os.getenv("CHANGJIANG_USER"),
     "changjiang_password": os.getenv("CHANGJIANG_PASSWORD"),
@@ -59,7 +61,9 @@ def userpwd_login(driver):
 
 # 钉钉机器人
 def DingMsg(Msg):
-    # please set charset= utf-8
+    global dmsg
+    if Msg==dmsg:
+        print("检测到重复发送同一条信息,已经停止这条信息的发送");return 1
     HEADERS = {
         "Content-Type": "application/json ;charset=utf-8 "
     }
@@ -76,11 +80,11 @@ def DingMsg(Msg):
     try:
         MessageBody = json.dumps(stringBody)
         result = requests.post(url=("https://oapi.dingtalk.com/robot/send?access_token="+os.getenv("DING_WEBHOOK_TOKEN")), data=MessageBody, headers=HEADERS)
-        print(os.getenv("DING_WEBHOOK_TOKEN"))
         print(result.text)
         if result.json().get('errcode') != 0:
             print("钉钉机器人发送错误:", result.json().get('errmsg'))
         print("已发送信息:",Msg)
+        dmsg = Msg
     except Exception as e:
         print("钉钉机器人发送错误:",e)
 
@@ -114,13 +118,70 @@ def login(driver):
             DingMsg("登陆失败,请更新cookie")
             exit(1)
 
+# 每节课中的节的处理
+def refresh_section(driver):
+    data={}
+    # 找到所有的section元素
+    sections = driver.find_elements_by_css_selector('section.nav__list.timeline__wrap')
+    # 遍历所有的section元素
+    for section in sections:
+        # 获取data-index属性
+        data_index = section.get_attribute('data-index')
+    # 找到嵌套的div元素
+    div = section.find_element_by_xpath('.//div')
+    # 获取div元素的title属性和内容
+    title = div.get_attribute('title')
+    content = div.text
+
+    # 保存数据
+    data[data_index] = {'title': title, 'content': content}
+    return data
+
+# 对弹幕列表的处理
+def refresh_danmu(driver):
+    # 如果短时间内大量重复弹幕，超过两条，自动发送同样内容弹幕
+    #<section data-v-2e8450a6="" class="danmu__container"><section data-v-2e8450a6="" class="danmu__wrap"><ul data-v-2e8450a6="" class="danmu__list"><li data-v-2e8450a6="" class="danmu__item J_danmu enter"><p data-v-2e8450a6="" class="danmu--text f12"><!----> <span data-v-2e8450a6="">1</span></p></li><li data-v-2e8450a6="" class="danmu__item J_danmu enter"><p data-v-2e8450a6="" class="danmu--text f12"><!----> <span data-v-2e8450a6="">1</span></p></li><li data-v-2e8450a6="" class="danmu__item J_danmu enter"><p data-v-2e8450a6="" class="danmu--text f12"><!----> <span data-v-2e8450a6="">1</span></p></li><li data-v-2e8450a6="" class="danmu__item J_danmu enter"><p data-v-2e8450a6="" class="danmu--text f12"><!----> <span data-v-2e8450a6="">1</span></p></li><li data-v-2e8450a6="" class="danmu__item J_danmu enter"><p data-v-2e8450a6="" class="danmu--text f12"><!----> <span data-v-2e8450a6="">1</span></p></li><li data-v-2e8450a6="" class="danmu__item J_danmu enter"><p data-v-2e8450a6="" class="danmu--text f12"><!----> <span data-v-2e8450a6="">2</span></p></li></ul></section></section>
+    #<input data-v-2e8450a6="" type="text" placeholder="说点什么" class="danmu__ipt J_input f12 cfff">
+    #<p data-v-76a19726="" title="弹幕" class="action__danmu box-center f12 cfff active">弹</p>
+    #<p data-v-76a19726="" title="弹幕" class="action__danmu box-center f12 cfff">弹</p>
+    #<p data-v-2e8450a6="" class="danmu__send box-center cfff">发送</p>
+    # 找到元素
+    element = driver.find_element_by_css_selector('p.action__danmu.box-center.f12.cfff')
+    # 检查类是否包含active
+    if 'active' not in element.get_attribute('class').split():
+        # 如果不包含，点击元素
+        element.click()
+    # 检测弹幕列表
+    # 找到所有的span元素
+    spans = driver.find_elements_by_xpath('//section[@class="danmu__container"]/section[@class="danmu__wrap"]//span')
+    # 创建一个列表来保存数据
+    data = []
+    # 遍历所有的span元素
+    for span in spans:
+        # 获取span元素的文本
+        text = span.text
+        data.append(text)
+        print(data)
+    from collections import Counter
+    # 统计data列表中每个元素出现的次数
+    counter = Counter(data)
+    # 找到出现次数最多的元素
+    most_common_element, count = counter.most_common(1)[0]
+    # 检查出现次数是否大于2
+    if count > 2:
+        print(most_common_element)
+        # 找到元素并填充
+        driver.find_element_by_css_selector('input.danmu__ipt.J_input').send_keys(most_common_element)
+        # 发送弹幕
+        driver.find_element_by_css_selector('p.danmu__send.box-center').click()
+
 def inlesson(driver):
     try:
         # 保存当前窗口的句柄
         current_window = driver.current_window_handle
         isLesson = False
-        DingMsg("雨课堂开始上课了,10秒后自动签到")
-        sleep(10)
+        section_data={}
+        sleep(20)
         try:
             driver.find_element_by_xpath('//div[@class="name-box"]/span[@class="name"]').click()
             # 切换到新的窗口
@@ -143,26 +204,28 @@ def inlesson(driver):
         while(1):
             sleep(10)
             driver.refresh()
-            # 如果短时间内大量重复弹幕，超过两条
-            #<section data-v-2e8450a6="" class="danmu__container"><section data-v-2e8450a6="" class="danmu__wrap"><ul data-v-2e8450a6="" class="danmu__list"><li data-v-2e8450a6="" class="danmu__item J_danmu enter"><p data-v-2e8450a6="" class="danmu--text f12"><!----> <span data-v-2e8450a6="">1</span></p></li><li data-v-2e8450a6="" class="danmu__item J_danmu enter"><p data-v-2e8450a6="" class="danmu--text f12"><!----> <span data-v-2e8450a6="">1</span></p></li><li data-v-2e8450a6="" class="danmu__item J_danmu enter"><p data-v-2e8450a6="" class="danmu--text f12"><!----> <span data-v-2e8450a6="">1</span></p></li><li data-v-2e8450a6="" class="danmu__item J_danmu enter"><p data-v-2e8450a6="" class="danmu--text f12"><!----> <span data-v-2e8450a6="">1</span></p></li><li data-v-2e8450a6="" class="danmu__item J_danmu enter"><p data-v-2e8450a6="" class="danmu--text f12"><!----> <span data-v-2e8450a6="">1</span></p></li><li data-v-2e8450a6="" class="danmu__item J_danmu enter"><p data-v-2e8450a6="" class="danmu--text f12"><!----> <span data-v-2e8450a6="">2</span></p></li></ul></section></section>
-            #<input data-v-2e8450a6="" type="text" placeholder="说点什么" class="danmu__ipt J_input f12 cfff">
-            #<p data-v-76a19726="" title="弹幕" class="action__danmu box-center f12 cfff active">弹</p>
-            #<p data-v-76a19726="" title="弹幕" class="action__danmu box-center f12 cfff">弹</p>
-            #<p data-v-2e8450a6="" class="danmu__send box-center cfff">发送</p>
+            # 目前没必要使用这个功能
+            #section_data=refresh_section()
+            #for each in section_data:
+            #    data_index = each
+            refresh_danmu(driver)
             # 如果有题目发布
-            if driver.find_elements_by_xpath('//div[@class="name-box"]/span[@class="name"]'):
-                DingMsg("有题目发布，距离截止")
+            if driver.find_elements_by_css_selector('div.time-box'):
+                DingMsg("有题目发布，"+driver.find_element_by_css_selector('div.time-box > div.timing.timing--number').text)
             # 如果有随机点名
             #<div title="随机点名选中：傅玉权2306020108" class="timeline__msg f12">随机点名选中：傅玉权2306020108</div>
-            if driver.find_elements_by_xpath('//div[@class="name-box"]/span[@class="name"]'):
-                DingMsg("课程结束")
+            if driver.find_elements_by_xpath('//div[starts-with(@title, "随机点名选中：")]'):
+                DingMsg(driver.find_element_by_xpath('//div[starts-with(@title, "随机点名选中：")]').text)
             # 如果有下课的提示
             #<div title="下课啦！" class="timeline__msg f12">下课啦！</div>
-            if driver.find_element_by_xpath('//div[@title="下课啦！" and text()="上课啦！"]'):
+            if driver.find_elements_by_xpath('//div[@title="下课啦！" and text()="下课啦！"]'):
                 DingMsg("课程结束")
                 break
+        driver.close()
         driver.switch_to.windows(current_window)
     except Exception as e:
+        DingMsg("上课时碰到问题"+e)
+        driver.switch_to.windows(current_window)
         pass
 
 def main():
@@ -170,7 +233,9 @@ def main():
     if os.getenv("RUNNING_IN_DOCKER"):
         try:
             sleep(10)
-            driver = webdriver.Remote(os.getenv("REMOTE_FIREFOX"), DesiredCapabilities.FIREFOX)
+            options = webdriver.FirefoxOptions()
+            options.set_preference("intl.accept_languages", "zh-CN")
+            driver = webdriver.Remote(os.getenv("REMOTE_FIREFOX"), DesiredCapabilities.FIREFOX,options=options)
         except Exception as e:
             DingMsg("连接远程selenium出错:", e)
     else:
@@ -189,14 +254,54 @@ def main():
         driver.get(r'https://changjiang.yuketang.cn/v2/web/index')
 
         DingMsg("监测开始")
-        i=0
+        cre_i=0
+        #<div class="name-box"><span class="tag">考</span> <span class="name">第一次数模实战</span></div>
+        #<div class="name-box"><span class="tag">听</span> <span class="name">第一次数模实战</span></div>
+        #<div class="name-box"><span class="tag">教</span> <span class="name">第一次数模实战</span></div>
+        #如果有多个项目会出现<i class="iconfont icon icon--shuangjiantouxiangxia"></i>
         while(1):
-            i+=1
-            if driver.find_elements_by_xpath('//div[@class="name-box"]/span[@class="name"]'):
-                inlesson(driver)
+            cre_i+=1
+            # 找到name-box元素
+            #name_box =   driver.find_elements_by_css_selector('.name-box')
+            name_box=0
+            both_class = driver.find_elements_by_css_selector('.iconfont.icon.icon--shuangjiantouxiangxia')
+            # 检查是否找到了name-box元素
+            if name_box or both_class:
+                if both_class:
+                    #<div class="onlessonlist"><div class="lessonlist"><div class="left"><p class="lessonTitle">s</p> <span class="status">教</span> <span class="teacher">医疗产品管理一班</span></div> <div class="right"><i class="iconfont icon--yinpinbofang"></i></div></div> <div class="lessonlist"><div class="left"><p class="lessonTitle">第一次数模实战</p> <span class="status">考试</span> <span class="teacher">校创数模学习小组</span></div> <div class="right"><i class="iconfont icon--danjiantouxiangyou f16"></i></div></div></div>
+                    both_class[0].click()
+                    sleep(2)
+                    # 找到onlessonlist元素
+                    onlessonlist = driver.find_element_by_css_selector('.onlessonlist')
+                    onless_lessonlists = onlessonlist.find_elements_by_css_selector('.lessonlist>.left')
+                    # 创建列表
+                    result = []
+                    for lessonlist in onless_lessonlists:
+                        # 找到所有的span元素
+                        lesson_spans = lessonlist.find_elements_by_tag_name('span')
+                        lesson_ps = lessonlist.find_element_by_tag_name('p')
+                        # 添加到结果列表
+                        result.append([lesson_spans[0].text, lesson_ps.text, lesson_spans[1].text])
+                    for i in result:
+                        if i[0]=='听':
+                            DingMsg("雨课堂开始上课了,这节课是"+i[1]+",20秒后自动签到。\n请注意,当前有多个课程："+str(result))
+                            inlesson(driver)
+                        elif i[0]=='考试':
+                            DingMsg("处于考试状态，将停止监控上课。当前正在考试科目："+i[2]+"\n请注意,当前有多个课程："+str(result))
+                elif name_box:
+                    # 找到所有的span元素
+                    spans = name_box[0].find_elements_by_tag_name('span')
+                    # 检查第一个span元素的文本是否为"考"
+                    if spans[0].text == "考":
+                        DingMsg("处于考试状态，将停止监控上课。当前正在考试科目："+spans[1].text)
+                    elif spans[0].text == '听':
+                        DingMsg("雨课堂开始上课了,10秒后自动签到")
+                        inlesson(driver)
             sleep(10)
+            name_box=0;both_class=0
             driver.refresh()
-            if i>=10:
+            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'tab-student')))
+            if cre_i>=10:
                 try:
                     check_cred_valid(driver)
                 except:
